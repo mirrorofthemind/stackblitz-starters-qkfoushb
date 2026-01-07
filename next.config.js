b9fc3b44cab593d35e35aa9ed59f7aaf080258c3
mirrorofthemind <ux.igormariano@gmail.com>
2026-01-07 15:35:07 -0300

    fix: correção de bug

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Isso aqui ignora completamente o ESLint durante o build na Vercel
+    // Isso ignora o erro de aspas (ESLint) durante o build na Vercel
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Isso aqui ignora erros de TypeScript que também podem travar o build
+    // Isso ignora erros de tipo que podem travar o build
     ignoreBuildErrors: true,
   },
 }
